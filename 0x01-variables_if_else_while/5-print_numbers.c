f460e97fbc94369aa68bca3da5e0544e3ef6440c
drmhz174 <dr.mhz174@gmail.com>
2023-09-12 17:01:20 +0300

    why

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main entery point
+ * Description: print numbers 1 to 9
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+	printf("%d\n", i);
+	}
+	return (0);
+}
