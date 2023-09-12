f9df35356938e3a90074797496c4419117537ab0
drmhz174 <dr.mhz174@gmail.com>
2023-09-13 01:04:05 +0300

    m

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - main entery point
+ *Description: A C prints __putchar
+ *Return:Always 0 (Success)
+*/
+int main(void)
+{
+	printf("__putchar");
+	printf("\n");
+	return (0);
+}
