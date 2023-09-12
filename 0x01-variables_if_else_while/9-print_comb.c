d0351348e7ac152229ee77277f63b45606b03005
drmhz174 <dr.mhz174@gmail.com>
2023-09-12 18:59:40 +0300

    mn

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - main entery point
+ *Description:AC prints
+ *Return:Always 0 (Success)
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+	putchar('0' + i);
+	putchar('0' + j);
+	if (i != 9 || j != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
