7daa9c0df3c551f4f11907e725e9199822266092
drmhz174 <dr.mhz174@gmail.com>
2023-09-21 21:36:24 +0300

    alias

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,13 @@ char *_strcat(char *dest, char *src)
 	int c, c2;
 
 	c = 0;
+	/*find the size of dest array*/
 
 	while (dest[c])
 	c++;
-	for (c2 = 0; src[c2] ; c++)
+
+	/*iterate through each src array value without the null byte*/
+	for (c2 = 0; src[c2] ; c2++)
 	dest[c++] = src[c2];
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -2,8 +2,9 @@
 
 /**
  * _putchar-write the char c to stdout
- * c: the character to print
- * Return : On success 1.
+ *@c: the character to print
+ *
+ *Return: On success 1.
  * On error, -1 is returned and errno is set appropriately.
  */
 int _putchar(char c)
