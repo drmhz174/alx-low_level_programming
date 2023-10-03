4ee75a94de583928c5db99959dbd22adb99bdd4c
¨drmhz174¨ <dr.mhz174@gmail.com>
2023-10-04 01:20:05 +0300

    ¨m¨

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  * @argv: list
  * Return: 0
  */
-int main(int argc, char const **argv)
+int main(int argc, char const *argv[])
 {(void) argc;
-	printf(¨%s\n,argv[0]);
-	return (0)
+	printf(¨ % s\n, argv[0]);
+	return (0);
 }
